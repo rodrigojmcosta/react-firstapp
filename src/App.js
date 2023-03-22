3c7b4eb8adfe4f5634cf5bb932ba18a45e66e3b8
Rodrigo J M Costa <rodrigo.costa@squadra.com.br>
2023-03-22 19:20:24 -0300

    40_Renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
-import Membro from "./components/Membro";
 
 class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: false
+     };
+
+     this.sair = this.sair.bind(this);
+     this.entrar = this.entrar.bind(this);
+}
+
+sair() {
+  this.setState({status: false});
+}
+
+entrar() {
+  this.setState({status: true});
+}
 
     render (){
       return(
         <div>
-            <Membro nome="Visitante"/>
+            {this.state.status?
+            <div>
+              <h2>Bem-vindo ao sistema</h2>
+              <button onClick={this.sair}> Sair do sistema</button>
+            </div> :
+            <div>
+              <h2>Olá Visitante, efetue o login para continuar.</h2>
+              <button onClick={this.entrar}> Entrar no sistema</button>
+            </div>
+            }
+
+
         </div>
       );
     }
