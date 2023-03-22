577e6f4ac7ff8b15bca672e53155655d228db15a
Rodrigo J M Costa <rodrigo.costa@squadra.com.br>
2023-03-22 17:42:08 -0300

    33pt2_Entendendo props e component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React from "react";
 
-const BemVindo = (props) => {
+const Equipe = (props) => {
+  return (
+    <div>
+      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
+      <Social fb={props.facebook}/>
+      <hr/>
+    </div>
+  );
+}
+
+const Sobre = (props) => {
     return(
       <div>
-        <h2>Bem-vindo(a) {props.nome}</h2>
-        <h3>Tenho {props.idade} anos </h3>
+        <h2>Olá sou o(a) {props.nome}</h2>
+        <h3>Cargo: {props.cargo}</h3>
+        <h3>Idade: {props.idade}</h3>
       </div>
     );
 }
 
+const Social = (props) => {
+  return(
+    <div>
+      <a href={props.fb}>Facebook </a>
+      <a>Linkedin </a>
+      <a>Youtube </a>
+    </div>
+    
+  );
+}
 
 function App() {
   return(
     <div>
-      Olá mundo!
-      <BemVindo nome="Rodrigo" idade="21"/>
-      <BemVindo nome="Wanderlúcio" idade="39"/>
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe nome="Rodrigo" cargo="Programador" idade="20"
+              facebook="https://facebook.com"/>
+      <Equipe nome="Lucas" cargo="Designer" idade="23"
+              facebook="https://google.com"/>
+      <Equipe nome="Amanda" cargo="Product Owner" idade="18"
+              facebook="https://google.com"/>
     </div>
   )
 }
